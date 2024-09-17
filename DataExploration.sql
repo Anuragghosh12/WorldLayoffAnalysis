7289ea01240db357a55241712508313e06fbe626
Anurag Ghosh <115475401+Anuragghosh12@users.noreply.github.com>
2024-09-18 01:04:53 +0530

    Update DataExploration.sql

--- DataExploration.sql
+++ DataExploration.sql
@@ -1,38 +1,38 @@
 SELECT * 
-FROM world_layoffs.layoffs_staging2;
+FROM world_layoffs.layoff_staging2;
 
 
 SELECT MAX(total_laid_off)
-FROM world_layoffs.layoffs_staging2;
+FROM world_layoffs.layoff_staging2;
 
 
 
 
 
 SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 WHERE  percentage_laid_off IS NOT NULL;
 
 SELECT *
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 WHERE  percentage_laid_off = 1;
 
 SELECT *
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 WHERE  percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 
 
 
 SELECT company, total_laid_off
-FROM world_layoffs.layoffs_staging
+FROM world_layoffs.layoff_staging
 ORDER BY 2 DESC
 LIMIT 5;
 
 
 -- Companies with the most Total Layoffs
 SELECT company, SUM(total_laid_off)
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 GROUP BY company
 ORDER BY 2 DESC
 LIMIT 10;
@@ -41,7 +41,7 @@ LIMIT 10;
 
 -- by location
 SELECT location, SUM(total_laid_off)
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 GROUP BY location
 ORDER BY 2 DESC
 LIMIT 10;
@@ -49,24 +49,24 @@ LIMIT 10;
 -- this it total in the past 3 years or in the dataset
 
 SELECT country, SUM(total_laid_off)
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 GROUP BY country
 ORDER BY 2 DESC;
 
 SELECT YEAR(date), SUM(total_laid_off)
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 GROUP BY YEAR(date)
 ORDER BY 1 ASC;
 
 
 SELECT industry, SUM(total_laid_off)
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 GROUP BY industry
 ORDER BY 2 DESC;
 
 
 SELECT stage, SUM(total_laid_off)
-FROM world_layoffs.layoffs_staging2
+FROM world_layoffs.layoff_staging2
 GROUP BY stage
 ORDER BY 2 DESC;
 
@@ -77,7 +77,7 @@ ORDER BY 2 DESC;
 WITH Company_Year AS 
 (
   SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
-  FROM layoffs_staging2
+  FROM layoff_staging2
   GROUP BY company, YEAR(date)
 )
 , Company_Year_Rank AS (
@@ -95,14 +95,14 @@ ORDER BY years ASC, total_laid_off DESC;
 
 -- Rolling Total of Layoffs Per Month
 SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
-FROM layoffs_staging2
+FROM layoff_staging2
 GROUP BY dates
 ORDER BY dates ASC;
 
 WITH DATE_CTE AS 
 (
 SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
-FROM layoffs_staging2
+FROM layoff_staging2
 GROUP BY dates
 ORDER BY dates ASC
 )
